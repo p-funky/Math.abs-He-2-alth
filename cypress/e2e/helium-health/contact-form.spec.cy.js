7b1ec0aa3ed5b5cbf1eb6ab8b30645f12404782c
p-funky <Adeyinka.Alabi@breadfinancial.com>
2023-07-24 23:03:49 +0100

    ch: add more tests

--- cypress/e2e/helium-health/contact-form.spec.cy.js
+++ cypress/e2e/helium-health/contact-form.spec.cy.js
@@ -16,9 +16,30 @@ describe("Helium Health Contact Form Test Suite", () => {
     contactPage.verifySubmitButtonPresent();
   });
 
-  it("should show validation messages when form is submitted without input", () => {
+  it("should show validation messages when form is submitted without input",
+    () => {
     contactPage.verifySubmitButtonPresent();
     contactPage.validateFormSubmissionWithoutInput();
   });
-  
+
+  it("should submit the form with valid input and show success message", () => {
+    contactPage.submitFormWithValidInput();
+  });
+
+  it("should show validation message for invalid emails", () => {
+    const invalidEmails = ["invalid-email", "john.doe@invalid", "@example.com"];
+    invalidEmails.forEach(email => {
+      contactPage.submitFormWithInvalidEmail(email);
+    });
+  });
+
+  it("should only select the first 10 digits of a phone number with more than 10 digits", () => {
+    const phoneWithMoreThan10Digits = "1234567890123";
+    contactPage.selectOnlyFirst10DigitsOfPhone(phoneWithMoreThan10Digits);
+  });
+
+  it("should not allow you to input a phone number less than 10 digits", () => {
+    const phoneWithLessThan10Digits = "123456789";
+    contactPage.discardPhoneWithLessThan10Digits(phoneWithLessThan10Digits);
+  });
 });
--- cypress/page-objects/contact-page.js
+++ cypress/page-objects/contact-page.js
@@ -7,10 +7,11 @@ export class ContactPage {
   hospital = "#input_1_7";
   location = "#input_1_9";
   noOfPatients = "#input_1_10";
-  message = "#field_1_11";
+  message = "#input_1_11";
   submitBtn = "#gform_submit_button_1";
   formValidationContainer = "#gform_1_validation_container";
-  generalFormErrorMessage = "There was a problem with your submission. Please review the fields below.";
+  generalFormErrorMessage =
+    "There was a problem with your submission. Please review the fields below.";
   firstNameValidation = "#validation_message_1_1";
   lastNameValidation = "#validation_message_1_2";
   emailValidation = "#validation_message_1_5";
@@ -20,6 +21,7 @@ export class ContactPage {
   locationValidation = "#validation_message_1_9";
   noOfPatientsValidation = "#validation_message_1_10";
   messageValidation = "#validation_message_1_11";
+  confirmationMessage = "#gform_confirmation_message_1";
 
   navigate() {
     // visit the website
@@ -55,7 +57,8 @@ export class ContactPage {
   validateFormSubmissionWithoutInput() {
     // submit the form without entering any information
     this.clickSubmitButton();
-    cy.get(this.formValidationContainer).should("be.visible").contains(this.generalFormErrorMessage);
+    cy.get(this.formValidationContainer).should("be.visible")
+      .contains(this.generalFormErrorMessage);
     // verify validation messages for each required field
     const oneFieldErrorMessage = "At least one field must be filled out";
     cy.get(this.firstNameValidation).should("contain", oneFieldErrorMessage);
@@ -67,5 +70,90 @@ export class ContactPage {
     cy.get(this.locationValidation).should("contain", oneFieldErrorMessage);
     cy.get(this.noOfPatientsValidation).should("contain", oneFieldErrorMessage);
     cy.get(this.messageValidation).should("contain", oneFieldErrorMessage);
+  }
+
+  fillFirstname(name = "John") {
+    cy.get(this.firstName).type(`${name}`, { force: true });
+  }
+
+  fillLastname(name = "Doe") {
+    cy.get(this.lastName).type(`${name}`, { force: true });
+  }
+
+  fillEmail(emailInput = "john.doe@example.com") {
+    cy.get(this.email).type(`${emailInput}`, { force: true });
+  }
+
+  fillPhone(phoneInput = "1234567890") {
+    cy.get(this.phone).type(`${phoneInput}`, { force: true });
+  }
+
+  fillHospital(name = "Saving Lives") {
+    cy.get(this.hospital).type(`${name}`, { force: true });
+  }
+
+  fillLocation(name = "Warri") {
+    cy.get(this.location).type(`${name}`, { force: true });
+  }
+
+  fillNumberOfPatients(num = 365) {
+    cy.get(this.noOfPatients).type(`${num}`, { force: true });
+  }
+
+  fillMessage(text = "lorem ipsum") {
+    cy.get(this.message).type(`${text}`, { force: true });
+  }
+
+  confirmSubmission() {
+    cy.get(this.confirmationMessage)
+      .should(
+        "contain",
+        "Thanks for contacting us! We will get in touch with you shortly."
+      );
+  }
+
+  submitFormWithValidInput() {
+    // fill in the form fields with valid input
+    this.fillFirstname();
+    this.fillLastname();
+    this.fillEmail();
+    this.fillPhone();
+    this.fillHospital();
+    this.fillLocation();
+    this.fillNumberOfPatients();
+    this.fillMessage();
+    // submit the form
+    this.clickSubmitButton();
+    // verify the success message
+    this.confirmSubmission();
+  }
+
+  submitFormWithInvalidEmail(email) {
+    // fill in the form fields with invalid email
+    this.fillEmail(email);
+    // submit the form
+    this.clickSubmitButton();
+    // validate the error message
+    cy.get(this.emailValidation).should(
+      "contain",
+      "The email address entered is invalid, please check the formatting (e.g. email@domain.com)."
+    );
+  }
+
+  selectOnlyFirst10DigitsOfPhone(phone) {
+    this.fillPhone(phone);
+    cy.get(this.phone).should(
+      "have.value",
+      // format (123) 456-7890
+      `(${phone.slice(0, 3)}) ${phone.slice(3, 6)}-${phone.slice(6, 10)}`
+    );
+  }
+
+  discardPhoneWithLessThan10Digits(phone) {
+    this.fillPhone(phone);
+    // click outside
+    cy.get('body').click(0, 0);
+    // check phone value
+    cy.get(this.phone).should('have.value', "");
   }
 }
